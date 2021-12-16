6a4b62cf2eb78cec36def3a776367b716097272a
die-kreatur <parusmajor@list.ru>
2021-12-17 00:23:46 +0300

    Created api view for active quizes

--- service/api/views.py
+++ service/api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from .serializers import QuizSerializer
+from .models import Quiz
 
-# Create your views here.
+
+class QuizList(generics.ListAPIView):
+    """View для получения списка активных опросов"""
+    queryset = Quiz.objects.all()    
+    serializer_class = QuizSerializer
+
+    def list(self, request):
+        """Метод для отсортировки только активных опросов для вывода"""
+        queryset = self.get_queryset()
+        queryset = list(filter(Quiz.is_active, queryset))
+        serializer = QuizSerializer(queryset, many=True)
+
+        return Response(serializer.data)
