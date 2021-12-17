a82933e705090c98c4ef2f4c214bade97fe85879
die-kreatur <parusmajor@list.ru>
2021-12-17 17:21:07 +0300

    Create API views

--- service/api/views.py
+++ service/api/views.py
@@ -1,18 +1,67 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.response import Response
-from .serializers import QuizSerializer
-from .models import Quiz
+from rest_framework.views import APIView
+from django.utils import timezone
+from django.db import IntegrityError
+from .serializers import QuestionSerializer, QuizSerializer, AnswerSerializer
+from .models import Quiz, Question, Answer
 
 
 class QuizList(generics.ListAPIView):
     """View для получения списка активных опросов"""
-    queryset = Quiz.objects.all()    
+    today = timezone.now().date()
+    queryset = Quiz.objects.filter(end__gte=today)
     serializer_class = QuizSerializer
 
-    def list(self, request):
-        """Метод для отсортировки только активных опросов для вывода"""
-        queryset = self.get_queryset()
-        queryset = list(filter(Quiz.is_active, queryset))
-        serializer = QuizSerializer(queryset, many=True)
 
-        return Response(serializer.data)
+class PostAnswers(APIView):
+    """View для ответа на вопросы"""
+    def get(self, request, pk, format=None):
+        """Получение вопроса по его идентификатору"""
+        try:
+            question = Question.objects.get(pk=pk)
+            serializer = QuestionSerializer(question, many=False)
+            return Response(serializer.data)
+        except Question.DoesNotExist:
+            return Response(
+                {'Error': f'Question number {pk} does not exist'},
+                status=status.HTTP_405_METHOD_NOT_ALLOWED
+                )
+
+
+    def post(self, request, pk, format=None):
+        """Отправка ответа на вопрос серверу и сохранение его в БД"""
+        try:    
+            question = Question.objects.get(pk=pk)
+            quiz = question.quiz_id
+            
+            request.data['question'] = question.pk
+            request.data['quiz'] = quiz
+
+            serializer = AnswerSerializer(data=request.data)
+
+            if serializer.is_valid():
+                serializer.save(
+                    uid=request.session['user_id']
+                )
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        except IntegrityError:
+            """Обработка ошибки, в случае если пользователь попытается ответить на вопрос снова"""
+            return Response(
+                {'Error': 'You have already answered this question'},
+                status=status.HTTP_405_NOT_FOUND
+                )
+
+# {"answer": "Rome"}
+
+
+class GetAnswers(generics.ListAPIView):
+    """Получение своих детализированныйх ответов пользователем на вопросы"""
+    serializer_class = AnswerSerializer
+
+    def get_queryset(self):
+        user = self.request.session['user_id']
+        return Answer.objects.filter(uid=user)
--- service/service/urls.py
+++ service/service/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from api.views import QuizList
+from api.views import QuizList, PostAnswers, GetAnswers
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/active-quizes', QuizList.as_view(), name='active-quizes')
+    path('api/active-quizes', QuizList.as_view(), name='active-quizes'),
+    path('api/questions/<int:pk>', PostAnswers.as_view(), name='post-answer'),
+    path('api/answers/', GetAnswers.as_view(), name='get-answers')
 ]
